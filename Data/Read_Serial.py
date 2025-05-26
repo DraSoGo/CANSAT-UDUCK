cfb22418d05bf6284aa584b2e2c606b81acc862c
DraSoGo <vdhfmfatv4321@gmail.com>
2025-05-26 15:30:57 +0700

    26/05/2025

--- Data/Read_Serial.py
+++ Data/Read_Serial.py
@@ -2,9 +2,9 @@ import serial
 import time
 from datetime import datetime
 
-ser = serial.Serial('/dev/ttyACM1', 9600)
+ser = serial.Serial('/dev/ttyACM0', 9600)
 
-with open("/home/drasogun/DraSoGun/Work/CANSAT_UDUCK/Hardware/Data/log.txt", "w") as f:
+with open("log.txt", "w") as f:
     while True:
         line = ser.readline().decode('utf-8', errors='replace').strip()
         if line == "-------------":
